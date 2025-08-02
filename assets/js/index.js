01bd55cb6cd4ec6ae42f4d47024f889f7dd977a8
IrynaKr1 <krutsiryna@gmail.com>
2025-08-02 12:18:16 +0200

    Change getter and ad hard task answer. Closes #1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
+const HASHTAG_LIST = ['1', '2', '3', '4', '5', '6'];
+const TAG_COLORS = {
+  1: 'red',
+  2: 'blue',
+  3: 'green',
+  4: 'orange',
+  5: 'purple',
+  6: 'brown',
+};
+
 class Post {
   constructor(id, name, author, text, addData, likes, img, hashtagList) {
     this._id = id;
@@ -7,7 +17,7 @@ class Post {
     this._addData = addData;
     this.likes = likes;
     this._img = img;
-    this._hashtagList = hashtagList;
+    this.hashtagList = hashtagList;
   }
 
   set likes(value) {
@@ -21,7 +31,47 @@ class Post {
   }
 
   get likes() {
-    return this.likes;
+    return this._likes;
+  }
+
+  set hashtagList(hashtags) {
+    this._hashtagList = [];
+    try {
+      hashtags.forEach((e) => this.addHashtag(e));
+    } catch (error) {
+      throw new Error('Hashtag list will not be created');
+    }
+  }
+
+  get hashtagList() {
+    return this._hashtagList;
+  }
+
+  addHashtag(hashtag) {
+    if (this._hashtagList.length >= 6) {
+      throw new RangeError('Size > 6');
+    }
+
+    if (!HASHTAG_LIST.includes(hashtag)) {
+      throw new RangeError('No in the list');
+    }
+
+    if (this._hashtagList.includes(hashtag)) {
+      throw new RangeError(
+        'Hashtag is already on the list. Duplicates is not allowed'
+      );
+    }
+
+    this._hashtagList.push(hashtag);
+  }
+
+  renderHashtags() {
+    return this._hashtagList
+      .map(
+        (hashtag) =>
+          `<span class="hashtag" style="color: ${TAG_COLORS[hashtag]};">#${hashtag}</span>`
+      )
+      .join(' ');
   }
 
   changeText(newText) {
@@ -54,6 +104,8 @@ class Post {
       _hashtagList: hashtagList,
     } = this;
 
+    const coloredHashtags = this.renderHashtags();
+
     return document.write(`
         <article class="userPost">
       <div class="imgWrapper">
@@ -70,7 +122,7 @@ class Post {
       <p class="text">
         ${postText}
       </p>
-      <p class="hashTags">${hashtagList}</p>
+      <p class="hashTags">${coloredHashtags}</p>
       <div class="likes">
         <i class="fa-regular fa-thumbs-up"></i>
         <span class="likesT">${likes}</span>
@@ -95,12 +147,14 @@ try {
     2024,
     2,
     'https://images.unsplash.com/photo-1752350434950-50e8df9c268e?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    ['one', 'two', 'three', 'four', 'five', 'six']
+    ['1', '2', '3', '4', '5', '6']
   );
   //post1.changeText('New text lorem bla bla bla');
   //post1.likesIncrease();
   post1.likesDecrease();
   post1.render();
+  console.log(post1);
+  // post1.addHashtag('5');
   console.log(post1);
 } catch (error) {
   console.error(error);
